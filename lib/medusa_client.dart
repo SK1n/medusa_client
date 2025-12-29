6a84720ad28a9b22761597024b6e5b70fd4f662e
SK1n <luys50734@gmail.com>
2025-12-29 22:22:12 +0200

    added ignores

--- lib/medusa_client.dart
+++ lib/medusa_client.dart
@@ -1,6 +1,7 @@
 ///
-library;
+library medusa_client;
 
 export 'src/medusa.dart';
 export 'src/medusa_store_client.dart';
+// If this line is missing, the Kiosk app will NEVER see the models
 export 'src/models/models.dart';
