25357848413eac8b5facf01e6638d2fdeb63bdfd
SK1n <luys50734@gmail.com>
2025-12-29 22:11:07 +0200

    fixed export files

--- lib/medusa_client.dart
+++ lib/medusa_client.dart
@@ -1,26 +1,3 @@
-import 'package:dio/dio.dart';
-import 'package:medusa_client/src/interceptors/medusa_error_interceptor.dart';
-import 'package:medusa_client/src/interceptors/medusa_store_interceptor.dart';
-import 'package:medusa_client/src/medusa_store_client.dart';
-
-class MedusaClient {
-  MedusaClient({
-    required String baseUrl,
-    required String publishableKey,
-    Duration timeout = const Duration(seconds: 30),
-  }) : _dio = Dio(
-         BaseOptions(
-           baseUrl: baseUrl,
-           connectTimeout: timeout,
-           receiveTimeout: timeout,
-         ),
-       ) {
-    _dio.interceptors.addAll([
-      MedusaStoreInterceptor(publishableKey),
-      MedusaErrorInterceptor(),
-    ]);
-    store = MedusaStoreClient(_dio);
-  }
-  late final MedusaStoreClient store;
-  final Dio _dio;
-}
+export 'src/medusa.dart';
+export 'src/medusa_store_client.dart';
+export 'src/models/models.dart';
