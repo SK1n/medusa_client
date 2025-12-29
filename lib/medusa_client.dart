7dd31be10e05fe861ad5c3b345c5415961a88180
SK1n <luys50734@gmail.com>
2025-12-29 22:22:28 +0200

    removed comments

--- lib/medusa_client.dart
+++ lib/medusa_client.dart
@@ -3,5 +3,4 @@ library medusa_client;
 
 export 'src/medusa.dart';
 export 'src/medusa_store_client.dart';
-// If this line is missing, the Kiosk app will NEVER see the models
 export 'src/models/models.dart';
